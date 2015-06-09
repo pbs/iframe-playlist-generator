ba2e543fe74734604a77ed3fa9d53356b5a38cb9
Cristina Botez <cristina.botez@3pillarglobal.com>
2015-06-09 11:37:12 +0300

    Switched back to out internal m3u8-0.1.8 because it is not compatible with the latest official m3u8-0.2.4

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name='iframe-playlist-generator',
-    version='0.1.1',
+    version='0.1.2',
     author='Peter Norton',
     author_email='peter@nortoncrew.com',
     packages=['iframeplaylistgenerator'],
     url='https://github.com/pbs/iframe-playlist-generator',
     description='HLS I-frame playlist generator',
     long_description=open('README.rst').read(),
-    install_requires=['m3u8==0.1.7b', 'subprocess32==3.2.6'],
-    dependency_links=['https://github.com/pbs/m3u8/tarball/master#egg=m3u8-0.1.7b'],
+    install_requires=['m3u8==0.1.8', 'subprocess32==3.2.6'],
 )
